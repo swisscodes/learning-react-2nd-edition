41bf59736eb355b6eda380d725f55f0b891cd23e
Swissbobo <swissbobomail.com>
2021-03-12 14:11:44 +0300

    added files i mistakely deleted

---
+++ src/components/StarRating.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Star from "./Star";
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({totalStars = 5, selectedStars = 0, onRate = f => f}) {
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => onRate(i + 1)}
+        />
+      ))}
+    </>
+  );
+}
